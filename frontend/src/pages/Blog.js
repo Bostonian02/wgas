485329ec7d8e5e5465e3cf9215ca5d1ccf683d8c
Bostonian02 <bostonian02@gmail.com>
2023-05-08 13:18:21 -0400

    Update img path

--- frontend/src/pages/Blog.js
+++ frontend/src/pages/Blog.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { useState, state } from 'react';
 import '../stylesheets/Main.css';
 import '../stylesheets/Blog.css';
-import '../images/Logo.png';
-import Navbar from '../components/Navbar.js'
 import PostPreview from '../components/PostPreview';
 
 function Blog() {
@@ -13,11 +11,10 @@ function Blog() {
         <div id="blogPageContainer">
             <input type="text" id="searchInput" value={searchText} placeholder={"Search..."} onChange={e => setSearchText(e.target.value)}/><br/>
             <div id="blogPostsContainer">
-                <PostPreview post={{ id: "235132352353", img_url: "Logo.png", title: "Why you should hire me", date: "April 20, 1969", tags: ["Nature", "Tech", "Movies"] }}/>
-                <PostPreview post={{ id: "235132352353", img_url: "../images/Logo.png", title: "Look at this cool art my best friend made", date: "April 20, 1969", tags: ["Projects", "Personal"] }}/>
-                <PostPreview post={{ id: "235132352353", img_url: "../images/Logo.png", title: "Drop out of college RIGHT NOW", date: "April 20, 1969", tags: ["School"] }}/>
-                <PostPreview post={{ id: "235132352353", img_url: "../images/Logo.png", title: "The Last of Us is a good show", date: "April 20, 1969", tags: ["Shows", "Games"] }}/>
-
+                <PostPreview post={{ id: "235132352353", img_url: "/images/forest.jpg", title: "Why you should hire me", date: "April 20, 1969", tags: ["Nature", "Tech", "Movies"] }}/>
+                <PostPreview post={{ id: "235132352353", img_url: "/images/Spanish Garden.png", title: "Look at this cool art my best friend made", date: "April 20, 1969", tags: ["Projects", "Personal"] }}/>
+                <PostPreview post={{ id: "235132352353", img_url: "/images/forest.jpg", title: "Drop out of college RIGHT NOW", date: "April 20, 1969", tags: ["School"] }}/>
+                <PostPreview post={{ id: "235132352353", img_url: "/images/forest.jpg", title: "The Last of Us is a good show", date: "April 20, 1969", tags: ["Shows", "Games"] }}/>
             </div>
         </div>
     );
