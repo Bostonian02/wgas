3e28702312393a9d19dc5b90abfacd3db56ca36f
Bostonian02 <bostonian02@gmail.com>
2023-05-08 03:44:56 -0400

    Update post preview styling and example data

--- frontend/src/pages/Blog.js
+++ frontend/src/pages/Blog.js
@@ -13,10 +13,10 @@ function Blog() {
         <div id="blogPageContainer">
             <input type="text" id="searchInput" value={searchText} placeholder={"Search..."} onChange={e => setSearchText(e.target.value)}/><br/>
             <div id="blogPostsContainer">
-                <PostPreview post={{ id: "235132352353", img_url: "Logo.png", title: "Test title", date: "April 20, 1969", tags: ["Nature"] }}/>
-                <PostPreview post={{ id: "235132352353", img_url: "../images/Logo.png", title: "Look at this cool art my best friend made", date: "April 20, 1969", tags: ["Nature"] }}/>
-                <PostPreview post={{ id: "235132352353", img_url: "../images/Logo.png", title: "Test title", date: "April 20, 1969", tags: ["Nature"] }}/>
-                <PostPreview post={{ id: "235132352353", img_url: "../images/Logo.png", title: "Test title", date: "April 20, 1969", tags: ["Nature"] }}/>
+                <PostPreview post={{ id: "235132352353", img_url: "Logo.png", title: "Why you should hire me", date: "April 20, 1969", tags: ["Nature", "Tech", "Movies"] }}/>
+                <PostPreview post={{ id: "235132352353", img_url: "../images/Logo.png", title: "Look at this cool art my best friend made", date: "April 20, 1969", tags: ["Projects", "Personal"] }}/>
+                <PostPreview post={{ id: "235132352353", img_url: "../images/Logo.png", title: "Drop out of college RIGHT NOW", date: "April 20, 1969", tags: ["School"] }}/>
+                <PostPreview post={{ id: "235132352353", img_url: "../images/Logo.png", title: "The Last of Us is a good show", date: "April 20, 1969", tags: ["Shows", "Games"] }}/>
 
             </div>
         </div>
