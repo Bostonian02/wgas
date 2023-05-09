7298f5925533a14fff790d1a0bd9a8bfb761a708
Bostonian02 <bostonian02@gmail.com>
2023-05-09 15:35:06 -0400

    Add get all posts endpoint

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -13,6 +13,22 @@ const db = new sqlite3.Database(db_name, (err) => {
     }
 });
 
+// Get all posts
+router.get('/', (req, res) => {
+    db.all('SELECT * FROM Posts', (err, rows) => {
+        if (err) {
+            res.status(500).send({ success: false, error: 'Error fetching posts from database' });
+        } else {
+            let rows_correct_tag = [];
+            rows.forEach((row) => {
+                let tags = row.tags.split(',');
+                rows_correct_tag.push({ id: row.id, title: row.title, date: row.date, body: row.body, preview_image_url: row.preview_image_url, tags: tags });
+            })
+            res.status(200).send({ success: true, posts: rows_correct_tag });
+        }
+    });
+});
+
 // Get a post based on its id
 router.get('/:id', (req, res) => {
     const id = req.params.id;
