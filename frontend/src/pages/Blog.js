939c0ae32dbf444214210482f92d873dbf6c3cc4
Bostonian02 <bostonian02@gmail.com>
2023-05-11 15:02:58 -0400

    Use lazy loading and add post search

--- frontend/src/pages/Blog.js
+++ frontend/src/pages/Blog.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { useState, state } from 'react';
 import '../stylesheets/Main.css';
 import '../stylesheets/Blog.css';
-import PostPreview from '../components/PostPreview';
+import buildPath from '../components/BuildPath';
+const PostPreview = React.lazy(() => import('../components/PostPreview'));
 
 function Blog() {
     const [isLoading, setLoading] = useState(true);
     const [searchText, setSearchText] = React.useState('');
     const [posts, setPosts] = React.useState([{}]);
 
+    const sortBlogPosts = (post1, post2) => {
+        return post2.id - post1.id;
+    }
+
     // Retrieve all posts from the database
     const getAllBlogPosts = async () => {
         try
         {
-            var response = await fetch('http://localhost:3001/api/posts/',
+            var response = await fetch(buildPath('/api/posts/'),
             { method: 'GET', headers: { 'Content-Type': 'application/json' } });
 
             var res = JSON.parse(await response.text());
@@ -51,17 +56,25 @@ function Blog() {
 
     return (
         <div id="blogPageContainer">
-            <input type="text" id="searchInput" value={searchText} placeholder={"Search..."} onChange={e => setSearchText(e.target.value)}/><br/>
+            <input type="text" id="searchInput" value={searchText} placeholder={"Search..."} onChange={(e) => {setSearchText(e.target.value);}}/><br/>
             <div id="blogPostsContainer">
                 {
-                    posts.map((post) => {
-                        return <PostPreview post={post}></PostPreview>;
+                    // Filter the posts based on search, sort them, then map the results
+                    posts.filter(post => {
+                        if (searchText === '') {
+                            return post;
+                        } else if (post.title.toLowerCase().includes(searchText.toLowerCase()) || post.date.toLowerCase().includes(searchText.toLowerCase())) {
+                            return post;
+                        }
+                    }).sort(sortBlogPosts).map((post, index) => {
+                        return (
+                            // Wrap the component in a 'Suspense' tag to lazy load it
+                            <Suspense fallback={ <span>Loading...</span> }>
+                                <PostPreview post={post} key={index}></PostPreview>
+                            </Suspense>
+                        );
                     })
                 }
-                {/* <PostPreview post={{ id: "1", preview_image_url: "/images/forest.jpg", title: "Why you should hire me", date: "April 20, 1969", tags: ["Nature", "Tech", "Movies"] }}/>
-                <PostPreview post={{ id: "235132352353", preview_image_url: "/images/Spanish Garden.png", title: "Look at this cool art my best friend made", date: "April 20, 1969", tags: ["Projects", "Personal"] }}/>
-                <PostPreview post={{ id: "235132352353", preview_image_url: "/images/forest.jpg", title: "Drop out of college RIGHT NOW", date: "April 20, 1969", tags: ["School"] }}/>
-                <PostPreview post={{ id: "235132352353", preview_image_url: "/images/forest.jpg", title: "The Last of Us is a good show", date: "April 20, 1969", tags: ["Shows", "Games"] }}/> */}
             </div>
         </div>
     );
