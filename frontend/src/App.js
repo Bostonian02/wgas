54b785225b2ec5c11c28592b36f5be83ecbde297
Bostonian02 <bostonian02@gmail.com>
2023-05-31 13:49:30 -0400

    Include link to "make a post" page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React, { Suspense } from 'react';
 // import Post from './pages/Post';
 import Navbar from './components/Navbar';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MakePost from './pages/MakePost';
 const Blog = React.lazy(() => import('./pages/Blog'));
 const Post = React.lazy(() => import('./pages/Post'));
 
@@ -15,6 +16,7 @@ function App() {
                 <Routes>
                     <Route path="/" index element={<Blog />} />
                     <Route path="/posts/" element={<Post />} />
+                    <Route path="/make-a-post-dumbass/" element={<MakePost />} />
                 </Routes>
             </Suspense>
         </BrowserRouter>
